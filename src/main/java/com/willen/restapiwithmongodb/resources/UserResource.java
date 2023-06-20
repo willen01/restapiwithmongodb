1bf8876055161d7024af1a09bcabb62eb533294c
willen <willen.sesantos@gmail.com>
2023-06-19 22:39:33 -0300

    Usado padrão DTO para retornar usuários

--- src/main/java/com/willen/restapiwithmongodb/resources/UserResource.java
+++ src/main/java/com/willen/restapiwithmongodb/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.willen.restapiwithmongodb.resources;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.willen.restapiwithmongodb.domain.User;
+import com.willen.restapiwithmongodb.dto.UserDTO;
 import com.willen.restapiwithmongodb.services.UserService;
 
 @RestController
@@ -19,10 +22,11 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         // responseentity: possui recursos mais adequados para uma resposta rest
-
         List<User> list = service.findall();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(listDto);
     }
 }
