f3adb7a53ed8630f09c6e775619fec65378b94e0
willen <willen.sesantos@gmail.com>
2023-06-23 09:31:06 -0300

    Consulta simples com @Query

--- src/main/java/com/willen/restapiwithmongodb/repository/PostRepository.java
+++ src/main/java/com/willen/restapiwithmongodb/repository/PostRepository.java
@@ -3,9 +3,15 @@ package com.willen.restapiwithmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.willen.restapiwithmongodb.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    // Consulta com método personalizado
+    @Query("{'title': { $regex: ?0, $options: 'i'}}")
+    List<Post> searchTitle(String text);
+
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
